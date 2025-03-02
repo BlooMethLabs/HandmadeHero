c29e629afbb72993b74be15f2cb975a40ee46d9a
BlooMethLabs <simply_barry@hotmail.com>
2025-03-02 15:25:58 +0000

    Platform abstraction memory managemnt

--- code/handmade.h
+++ code/handmade.h
@@ -1,5 +1,16 @@
 #if !defined(HANDMADE_H)
 
+#if HANDMADE_SLOW
+#define Assert(Expression) if (!(Expression)) {*(int *)0 = 0;}
+#else
+#define Assert(Expression)
+#endif
+
+#define Kilobytes(Value) ((Value)*1024LL)
+#define Megabytes(Value) (Kilobytes(Value)*1024LL)
+#define Gigabytes(Value) (Megabytes(Value)*1024LL)
+#define Terabytes(Value) (Gigabytes(Value)*1024LL)
+
 #define ArrayCount(Array) (sizeof(Array) / sizeof(Array[0]))
 struct game_offscreen_buffer
 {
@@ -58,10 +69,28 @@ struct game_input
     game_controller_input Controllers[4];
 };
 
-internal void GameUpdateAndRender(game_offscreen_buffer *Buffer,
-                                int BlueOffset,
-                                int GreenOffset,
-                                game_sound_output_buffer *SoundBuffer);
+struct game_memory
+{
+    bool32 IsInitialized;
+
+    uint64 PermanentStorageSize;
+    void *PermanentStorage;
+
+    uint64 TransientStorageSize;
+    void *TransientStorage;
+};
+
+internal void GameUpdateAndRender(game_memory *Memory,
+                                  game_input *Input,
+                                  game_offscreen_buffer *Buffer,
+                                  game_sound_output_buffer *SoundBuffer);
+
+struct game_state
+{
+    int ToneHz;
+    int GreenOffset;
+    int BlueOffset;
+};
 
 #define HANDMADE_H
 #endif
